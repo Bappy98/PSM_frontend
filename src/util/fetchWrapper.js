93101b17e0abd48fdbb1f0233bc4468ee23b0b66
Faysal Ahmed Bappy <faysalahmed.cse.98@gmail.com>
2025-06-21 15:41:43 +0600

    live fix

--- src/util/fetchWrapper.js
+++ src/util/fetchWrapper.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const fetchWrapper = axios.create({
-	baseURL: 'http://localhost:3000/api/v1/',
+	baseURL: import.meta.env.VITE_API_URL,
   
 	// timeout: 5000,
 	headers: {
